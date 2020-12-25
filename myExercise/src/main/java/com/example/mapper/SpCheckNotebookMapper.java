ada5ffe338d18126af4eb9f15c793642616e371c
wns wns <wangns@shipinginfo.com>
2020-12-25 18:02:35 +0800

    修改： mybatis-plus逆向工程生产代码

---
+++ myExercise/src/main/java/com/example/mapper/SpCheckNotebookMapper.java
@@ -0,0 +1,16 @@
+package com.example.mapper;
+
+import com.example.model.SpCheckNotebook;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author wns
+ * @since 2020-12-25
+ */
+public interface SpCheckNotebookMapper extends BaseMapper<SpCheckNotebook> {
+
+}
---
+++ myExercise/src/main/java/com/example/service/SpCheckNotebookService.java
@@ -0,0 +1,16 @@
+package com.example.service;
+
+import com.example.model.SpCheckNotebook;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author wns
+ * @since 2020-12-25
+ */
+public interface SpCheckNotebookService extends IService<SpCheckNotebook> {
+
+}
