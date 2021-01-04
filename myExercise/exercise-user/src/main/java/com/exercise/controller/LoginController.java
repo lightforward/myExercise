c1a6593a0f6ae73d0ec8a9f95d8fcae04cda76fd
wns wns <wangns@shipinginfo.com>
2021-01-04 17:55:30 +0800

    修改: 添加shiro

--- myExercise/exercise-user/src/main/java/com/exercise/controller/LoginController.java
+++ myExercise/exercise-user/src/main/java/com/exercise/controller/LoginController.java
@@ -2,9 +2,10 @@ package com.exercise.controller;
 
 import com.exercise.model.User;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.subject.Subject;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -14,7 +15,7 @@ import java.util.Map;
 /**
  * @Author: 王能顺
  * @Date: 2021-1-4 15:12:13
- *
+ * <p>
  * 用户登录注册的控制层
  */
 @RestController
@@ -28,8 +29,8 @@ public class LoginController {
      * @param request
      * @return
      */
-    @RequestMapping(value = "login",method= RequestMethod.POST)
-    public Object login(HttpServletRequest request) {
+    @RequestMapping(value = "loginUser",method= RequestMethod.POST)
+    public Object loginUser(HttpServletRequest request) {
         log.info("logback 访问:登录接口");
         String userName = request.getParameter("userName");
         String passWord = request.getParameter("passWord");
@@ -59,6 +60,40 @@ public class LoginController {
         //将数据存储到session中
         session.setAttribute("loginUser", loginUser);
         session.setMaxInactiveInterval(60 * 20); //单位秒
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @ResponseBody
+    public Object login(HttpServletRequest request) {
+        String userName = request.getParameter("userName");
+        String passWord = request.getParameter("passWord");
+        // 从SecurityUtils里边创建一个 subject
+        Subject subject = SecurityUtils.getSubject();
+        // 在认证提交前准备 token（令牌）
+        UsernamePasswordToken token = new UsernamePasswordToken(userName, passWord);
+        Map<Object, Object> result = new HashMap<Object, Object>();
+        // 执行认证登陆
+        try {
+            subject.login(token);
+        } catch (UnknownAccountException uae) {
+            return "未知账户";
+        } catch (IncorrectCredentialsException ice) {
+            return "密码不正确";
+        } catch (LockedAccountException lae) {
+            return "账户已锁定";
+        } catch (ExcessiveAttemptsException eae) {
+            return "用户名或密码错误次数过多";
+        } catch (AuthenticationException ae) {
+            return "用户名或密码不正确！";
+        }
+        if (subject.isAuthenticated()) {
+            result.put("resultCode", 200);
+            result.put("msg", "登录成功");
+            return result;
+        } else {
+            token.clear();
+            return "登录失败";
+        }
     }
 
 }
